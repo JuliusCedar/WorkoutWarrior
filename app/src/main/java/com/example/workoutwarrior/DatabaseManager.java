88a4034dbc3575d861639e09bfe280e28232b17e
Lucas Binder <lbinder500@gmail.com>
2021-02-09 13:34:38 -0800

    Added a database manager class

---
+++ app/src/main/java/com/example/workoutwarrior/DatabaseManager.java
@@ -0,0 +1,130 @@
+package com.example.workoutwarrior;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "warriorDB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String PROFILE_TABLE = "profileTable";
+    private static final String PLAYER_STATS = "playerStats";
+
+    public DatabaseManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /* Create two tables in the database. One for storing information relating to the user's profile
+       and another for keeping track of the players stats.
+     */
+    public void onCreate(SQLiteDatabase db) {
+        createProfileTable(db);
+        createStatsTable(db);
+    }
+
+    private void createProfileTable(SQLiteDatabase db) {
+        String createProfileTable = "create table " + PROFILE_TABLE + " ( ID integer primary key, ";
+        createProfileTable += ", name text, password text, class text)";
+        db.execSQL(createProfileTable);
+    }
+
+    private void createStatsTable(SQLiteDatabase db) {
+        String createStatsTable = "create table " + PLAYER_STATS + " (ID integer primary key, ";
+        createStatsTable += "level integer, strength integer, dexterity integer, constitution integer)";
+        db.execSQL(createStatsTable);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + PROFILE_TABLE);
+        db.execSQL("drop table if exists " + PLAYER_STATS);
+        onCreate(db);
+    }
+
+    /* Inserts a new row into the profile table.
+     */
+    public void insertIntoProfile(Profile profile, int id, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String insert = "insert into " + PROFILE_TABLE;
+        insert += " values( " + id + ", " + profile.getName() + ", " + password;
+        insert += ", " + profile.getClass() +  ")";
+        db.execSQL(insert);
+        db.close();
+    }
+
+    /* Inserts a new row into the stats table.
+     */
+    public void insertIntoStats(Profile profile, int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String insert = "insert into " + PLAYER_STATS;
+        insert += " values( " + id + ", " + profile.getLevel() + ", " + profile.getStr();
+        insert +=  ", " + profile.getDex() + ", " + profile.getCon() + ")";
+        db.execSQL(insert);
+        db.close();
+    }
+
+    /* Updates a player's stats table. Note that you have to give it every value
+     * even if one or more of them are unchanged.
+     */
+    public void updateStatsById(int id, Profile profile) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String update = "update " + PLAYER_STATS;
+        update += " set level = '" + profile.getLevel() + "', ";
+        update += "strength = '" + profile.getStr() + "', ";
+        update += "dexterity = '" + profile.getDex() + "', ";
+        update += "constitution = '" + profile.getCon() + "'";
+        update += " where ID = " + id;
+
+        db.execSQL(update);
+        db.close();
+    }
+
+    /* Returns a hashtable of values for a given profile based on the id.
+     * Example return value:   {"name": "BigMuscleGuy",
+     *                          "class": "Knight",
+     *                          "level": "30",
+     *                          "strength": "30",
+     *                          "dexterity": "3",
+     *                          "constitution": "7"}
+     */
+    public Dictionary selectById(int id) {
+        String[] profileInfo = getNameAndClass(id);
+        String getStats = " select * from " + PLAYER_STATS + " where id = " + id;
+        Dictionary playerStats = new Hashtable();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(getStats, null);
+
+        if (cursor.moveToFirst()) {
+            playerStats.put("name", profileInfo[0]);
+            playerStats.put("class", profileInfo[1]);
+            playerStats.put("level", cursor.getString(1));
+            playerStats.put("strength", cursor.getString(2));
+            playerStats.put("dexterity", cursor.getString(3));
+            playerStats.put("constitution", cursor.getString(4));
+        }
+
+        return playerStats;
+    }
+
+    /* Returns a players name and class in an array of Strings.
+     * The first index contains the player's name and the second
+     * index contains the player's class.
+     */
+    private String[] getNameAndClass(int id) {
+        String getProfile = "select * from " + PROFILE_TABLE + " where id = " + id;
+        String[] nameClass = new String[2];
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(getProfile, null);
+
+        if (cursor.moveToFirst()) {
+            nameClass[0] = cursor.getString(1);
+            nameClass[1] = cursor.getString(3);
+        }
+        return nameClass;
+    }
+}
