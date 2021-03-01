8f8a39f0b97854fdb2efe61c1c9ced7a32e898da
JuliusCedar <forest.heintz@outlook.com>
2021-03-01 13:16:52 -0800

    workout buttons open the correct activity, google maps started but not working

--- app/src/main/java/com/example/workoutwarrior/WorkoutFragment.java
+++ app/src/main/java/com/example/workoutwarrior/WorkoutFragment.java
@@ -1,5 +1,6 @@
 package com.example.workoutwarrior;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,5 +15,33 @@ public class WorkoutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_workout, null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View.OnClickListener buttonClicked = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.dexterity_workout_button:
+                        Intent dexIntent = new Intent(getActivity(), DexterityWorkoutActivity.class);
+                        getActivity().startActivity(dexIntent);
+                        break;
+                    case R.id.strength_workout_button:
+                        Intent strIntent = new Intent(getActivity(), StrengthWorkoutActivity.class);
+                        getActivity().startActivity(strIntent);
+                        break;
+                    case R.id.constitution_workout_button:
+                        Intent conIntent = new Intent(getActivity(), ConstitutionWorkoutActivity.class);
+                        getActivity().startActivity(conIntent);
+                        break;
+                }
+            }
+        };
+
+        getActivity().findViewById(R.id.dexterity_workout_button).setOnClickListener(buttonClicked);
+        getActivity().findViewById(R.id.strength_workout_button).setOnClickListener(buttonClicked);
+        getActivity().findViewById(R.id.constitution_workout_button).setOnClickListener(buttonClicked);
     }
 }
