8aa74d51266ff826e07131077f81316fc4c95cff
JuliusCedar <forest.heintz@outlook.com>
2021-03-01 14:08:20 -0800

    workouts should show correct tag text in workout fragment once database is set up correctly

--- app/src/main/java/com/example/workoutwarrior/WorkoutFragment.java
+++ app/src/main/java/com/example/workoutwarrior/WorkoutFragment.java
@@ -5,12 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class WorkoutFragment extends Fragment {
+
+    private static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static DatabaseReference workoutRef = database.getReference().child("quests");
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,28 +31,48 @@ public class WorkoutFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        View.OnClickListener buttonClicked = new View.OnClickListener() {
+
+        setupButton((Button) getActivity().findViewById(R.id.dexterity_workout_button));
+        setupButton((Button) getActivity().findViewById(R.id.strength_workout_button));
+        setupButton((Button) getActivity().findViewById(R.id.constitution_workout_button));
+    }
+
+    private void setupButton(Button button){
+        switch (button.getId()){
+            case R.id.dexterity_workout_button:
+                button.setOnClickListener(createOnWorkoutClicked(DexterityWorkoutActivity.class));
+                setButtonWorkoutTag(button, "dexterity", Profile.getProfile().getdQuest());
+                break;
+            case R.id.strength_workout_button:
+                button.setOnClickListener(createOnWorkoutClicked(StrengthWorkoutActivity.class));
+                setButtonWorkoutTag(button, "strength", Profile.getProfile().getsQuest());
+                break;
+            case R.id.constitution_workout_button:
+                button.setOnClickListener(createOnWorkoutClicked(ConstitutionWorkoutActivity.class));
+                setButtonWorkoutTag(button, "constitution", Profile.getProfile().getcQuest());
+                break;
+        }
+    }
+
+    private View.OnClickListener createOnWorkoutClicked(Class activityClass){
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.dexterity_workout_button:
-                        Intent dexIntent = new Intent(getActivity(), DexterityWorkoutActivity.class);
-                        getActivity().startActivity(dexIntent);
-                        break;
-                    case R.id.strength_workout_button:
-                        Intent strIntent = new Intent(getActivity(), StrengthWorkoutActivity.class);
-                        getActivity().startActivity(strIntent);
-                        break;
-                    case R.id.constitution_workout_button:
-                        Intent conIntent = new Intent(getActivity(), ConstitutionWorkoutActivity.class);
-                        getActivity().startActivity(conIntent);
-                        break;
-                }
+                Intent myIntent = new Intent(getActivity(), activityClass);
+                getActivity().startActivity(myIntent);
             }
         };
-
-        getActivity().findViewById(R.id.dexterity_workout_button).setOnClickListener(buttonClicked);
-        getActivity().findViewById(R.id.strength_workout_button).setOnClickListener(buttonClicked);
-        getActivity().findViewById(R.id.constitution_workout_button).setOnClickListener(buttonClicked);
     }
+
+    private void setButtonWorkoutTag(Button button, String workoutType, int level){
+        workoutRef.child(workoutType).child(""+level).child("tag").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                button.setText(String.valueOf(task.getResult().getValue()));
+            }
+        });
+    }
+
+
+
 }
