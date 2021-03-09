1d6f19391f0b39c68e8511e39dcc5aaf38474a27
JuliusCedar <forest.heintz@outlook.com>
2021-03-08 18:27:11 -0800

    Map now centers on devices location using GPS

--- app/src/main/java/com/example/workoutwarrior/DexterityWorkoutActivity.java
+++ app/src/main/java/com/example/workoutwarrior/DexterityWorkoutActivity.java
@@ -1,12 +1,21 @@
 package com.example.workoutwarrior;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,23 +28,34 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class DexterityWorkoutActivity extends FragmentActivity implements OnMapReadyCallback {
+public class DexterityWorkoutActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private final static int LOCATION_REQUEST_CODE = 1;
 
     private static FirebaseDatabase database = FirebaseDatabase.getInstance();
     private static DatabaseReference dRef = database.getReference().child("quests").child("dexterity");
 
+    private GoogleMap mMap;
+    LocationManager locationManager;
+
     private DexWorkoutHelper currentWorkout;
 
-    private GoogleMap mMap;
+    LatLng currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dexterity_workout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setupGPS();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         dRef.child(""+Profile.getProfile().getdQuest()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -45,7 +65,33 @@ public class DexterityWorkoutActivity extends FragmentActivity implements OnMapR
                 }
             }
         });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case LOCATION_REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    setupGPS();
+                }
+                else{
+                    //TODO: Permission denied, handle this
+                }
+                return;
+        }
+    }
+
+    private void setupGPS(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean finePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        boolean coursePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        if (finePermission && coursePermission) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+        else{
+            String[] neededPermissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(this, neededPermissions, LOCATION_REQUEST_CODE);
+        }
     }
 
     private void populateData(){
@@ -64,12 +110,8 @@ public class DexterityWorkoutActivity extends FragmentActivity implements OnMapR
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
 
     public void finishWorkout(View view){
         Profile.getProfile().finishDQuest();
@@ -80,5 +122,26 @@ public class DexterityWorkoutActivity extends FragmentActivity implements OnMapR
 
     public void goBack(View view){
         finish();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
